837bc12ffdcccd5beac52da759ede385cc7aa49a
Nguyenvanthinh272454254 <87877560+Nguyenvanthinh272454254@users.noreply.github.com>
2022-01-11 17:43:44 +0700

    Customizing how records are searched

--- 99.Odoo14/myAddon/my_library/models/library_book.py
+++ 99.Odoo14/myAddon/my_library/models/library_book.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-import logging
-
 from odoo import models, fields, api
-from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
 
-logger = logging.getLogger(__name__)
 
 
 class my_library(models.Model):
@@ -13,132 +9,31 @@ class my_library(models.Model):
     _description = 'Library Book'
 
     name = fields.Char('Title', required=True)
+    isbn = fields.Char('ISBN')
+
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
-    category_id = fields.Many2one('library.book.category', string='Category')
+    old_edition = fields.Many2one('library.book', string='Old Edition')
 
-    state = fields.Selection(
-        [('draft', 'Not Available'),
-         ('available', 'Available'),
-         ('lost', 'Lost')],
-        'State', default="draft")
-    
-    # Add a helper method to check whether a state transition is allowed
-    @api.model
-    def is_allowed_transition(self, old_state, new_state):
-        allowed = [('draft', 'available'),
-                   ('available', 'borrowed'),
-                   ('borrowed', 'available'),
-                   ('available', 'lost'),
-                   ('borrowed', 'lost'),
-                   ('lost', 'available')]
-        return (old_state, new_state) in allowed
-
-    # Add a method to change the state of some books to a new state that is passed as anargument
-    def change_state(self, new_state):
+    def name_get(self):
+        result = []
         for book in self:
-            if book.is_allowed_transition(book.state, new_state):
-                book.state = new_state
-            else:
-                continue
+            authors = book.author_ids.mapped('name')
+            name = '%s (%s)' % (book.name, ', '.join(authors))
+            result.append((book.id, name))
+        return result
 
-    # Add a method to change the book state by calling the change_state method:
-    def make_available(self):
-        self.change_state('available')
-
-    def make_borrowed(self):
-        self.change_state('borrowed')
-
-    def make_lost(self):
-        self.change_state('lost')
-
-    def log_all_library_members(self):
-        library_member_model = self.env['library.member']  # This is an empty recordset of model library.member
-        all_members = library_member_model.search([])
-        print("ALL MEMBERS:", all_members)
-        return True
-    
-    def create_categories(self):
-        categ1 = {
-            'name': 'Child category 1',
-            'description': 'Description for child 1'
-        }
-        categ2 = {
-            'name': 'Child category 2',
-            'description': 'Description for child 2'
-        }
-        parent_category_val = {
-            'name': 'Parent category',
-            'description': 'Description for parent category',
-            'child_ids': [
-                (0, 0, categ1),
-                (0, 0, categ2),
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = [] if args is None else args.copy()
+        if not(name == '' and operator == 'ilike'):
+            args += ['|', '|', '|',
+                ('name', operator, name),
+                ('isbn', operator, name),
+                ('author_ids.name', operator, name)
             ]
-        }
-        # Total 3 records (1 parent and 2 child) will be craeted in library.book.category model
-        record = self.env['library.book.category'].create(parent_category_val)
-        return True
-    
-    def change_release_date(self):
-        self.ensure_one()
-        self.date_release = fields.Date.today()
-    
-    def find_book(self):
-        domain = [
-            '|',
-                '&', ('name', 'ilike', 'Book Name'),
-                     ('category_id.name', '=', 'Category Name'),
-                '&', ('name', 'ilike', 'Book Name 2'),
-                     ('category_id.name', '=', 'Category Name 2')
-        ]
-        books = self.search(domain)
-        logger.info('Books found: %s', books)
-        return True
-    
-      # Filter recordset
-    def filter_books(self):
-        all_books = self.search([])
-        filtered_books = self.books_with_multiple_authors(all_books)
-        logger.info('Filtered Books: %s', filtered_books)
+        return super(my_library, self)._name_search(
+            name=name, args=args, operator=operator,
+            limit=limit, name_get_uid=name_get_uid)
 
-    @api.model
-    def books_with_multiple_authors(self, all_books):
-        def predicate(book):
-            if len(book.author_ids) > 1:
-                return True
-        return all_books.filtered(predicate)
-    
-    # Traversing recordset
-    def mapped_books(self):
-        all_books = self.search([])
-        books_authors = self.get_author_names(all_books)
-        logger.info('Books Authors: %s', books_authors)
-
-    @api.model
-    def get_author_names(self, all_books):
-        return all_books.mapped('author_ids.name')
-
-      # Sorting recordset
-    def sort_books(self):
-        all_books = self.search([])
-        books_sorted = self.sort_books_by_date(all_books)
-        logger.info('Books before sorting: %s', all_books)
-        logger.info('Books after sorting: %s', books_sorted)
-
-    @api.model
-    def sort_books_by_date(self, all_books):
-        return all_books.sorted(key='date_release')
-
-    
-
-class LibraryMember(models.Model):
-    _name = 'library.member'
-    _inherits = {'res.partner': 'partner_id'}
-
-    _description = 'Library Member'
-
-    partner_id = fields.Many2one('res.partner', required=True, ondelete='cascade')
-    date_start = fields.Date('Member Since')
-    date_end = fields.Date('Termination Date')
-    member_number = fields.Char()
-    date_of_birth = fields.Date('Date of birth')
+   
