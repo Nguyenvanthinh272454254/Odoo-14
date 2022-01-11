fab512986d7f21b9f2f2c52e2726c00d0fffc69e
Nguyenvanthinh272454254 <87877560+Nguyenvanthinh272454254@users.noreply.github.com>
2022-01-11 14:03:54 +0700

    done Defining model methods and using API decorators

--- 99.Odoo14/myAddon/my_library/models/library_book.py
+++ 99.Odoo14/myAddon/my_library/models/library_book.py
@@ -22,7 +22,7 @@ class my_library(models.Model):
     _description = 'Library Book'
     _order = 'date_release desc, name'
 
-    
+
     name = fields.Char('Title', required=True)
     short_name = fields.Char('Short Title',translate=True, index=True)
 
@@ -128,6 +128,38 @@ class my_library(models.Model):
         }
         new_op = operator_map.get(operator, operator)
         return [('date_release', new_op, value_date)]
+    
+    # Add a helper method to check whether a state transition is allowed
+    @api.model
+    def is_allowed_transition(self, old_state, new_state):
+        allowed = [('draft', 'available'),
+                   ('available', 'borrowed'),
+                   ('borrowed', 'available'),
+                   ('available', 'lost'),
+                   ('borrowed', 'lost'),
+                   ('lost', 'available')]
+        return (old_state, new_state) in allowed
+
+
+    # Add a method to change the state of some books to a new state that is passed as anargument
+    def change_state(self, new_state):
+        for book in self:
+            if book.is_allowed_transition(book.state, new_state):
+                book.state = new_state
+            else:
+                continue
+    
+
+    # Add a method to change the book state by calling the change_state method:
+    def make_available(self):
+        self.change_state('available')
+
+    def make_borrowed(self):
+        self.change_state('borrowed')
+
+    def make_lost(self):
+        self.change_state('lost')
+
 
 
 
