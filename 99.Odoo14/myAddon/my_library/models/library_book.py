08c3dd646f11a74e9556bcbc5dbdd438c8058947
Nguyenvanthinh272454254 <87877560+Nguyenvanthinh272454254@users.noreply.github.com>
2022-01-11 15:58:54 +0700

    200~09_traversing_recordset~

--- 99.Odoo14/myAddon/my_library/models/library_book.py
+++ 99.Odoo14/myAddon/my_library/models/library_book.py
@@ -107,6 +107,16 @@ class my_library(models.Model):
             if len(book.author_ids) > 1:
                 return True
         return all_books.filtered(predicate)
+    
+    # Traversing recordset
+    def mapped_books(self):
+        all_books = self.search([])
+        books_authors = self.get_author_names(all_books)
+        logger.info('Books Authors: %s', books_authors)
+
+    @api.model
+    def get_author_names(self, all_books):
+        return all_books.mapped('author_ids.name')
 
     
 
